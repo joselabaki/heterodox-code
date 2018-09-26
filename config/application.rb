a57d2fcefec23723b67d18e27f9ae424acbb7d7f
Jose Labaki <jose.labaki@hotmail.com>
2018-09-26 23:56:59 +0300

    Mod in config

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module HeterodoxCode
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "images")
 
     config.i18n.fallbacks = true
     
