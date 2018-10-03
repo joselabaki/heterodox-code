000df9c51012c5a2a35d68bb791fc3585a742ea5
Jose Labaki <jose.labaki@hotmail.com>
2018-10-03 16:23:05 +0300

    Navigation Clicks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,29 @@
 $(document).ready( function() {
 console.log("fire!")
 
+
 });
 
+$( document ).on('turbolinks:load', function() {
+    window.onload = $(function(){
+        var current = location.pathname;
+        $('.nav-items a').each(function(){
+            var $this = $(this);
+            // if the current path is like this link, make it active
+            if($this.attr('href').indexOf(current) !== -1){
+                $this.addClass('active');
+            }
+        })
+    })
+  })
 
+// $(function(){
+//     var current = location.pathname;
+//     $('.nav-items a').each(function(){
+//         var $this = $(this);
+//         // if the current path is like this link, make it active
+//         if($this.attr('href').indexOf(current) !== -1){
+//             $this.addClass('active');
+//         }
+//     })
+// })
