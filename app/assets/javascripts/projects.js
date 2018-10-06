efc87377b1cbd07f339f212f5462f49b25895625
Jose Labaki <jose.labaki@hotmail.com>
2018-10-06 17:08:39 +0300

    Modify

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,20 +2,18 @@ document.addEventListener("turbolinks:load", function() {
 $("#heterodox-logo").mouseenter(function(){
 $(".subtitle").removeClass("hidden")
 });
-    
+
 $("#heterodox-logo").mouseleave(function(){
     $(".subtitle").addClass("hidden")
     });
-            
-$(".centre").mouseenter(function(){
-$(".centre-texte").css("display", "block")
+
+$(".center").mouseenter(function(){
+$(".centre-texte").removeClass("hidden")
 });
 
-$(".centre").mouseleave(function(){
-    $(".centre-texte").css("display", "none")
+$(".center").mouseleave(function(){
+    $(".centre-texte").addClass("hidden")
     });
-
-
 
 
 
